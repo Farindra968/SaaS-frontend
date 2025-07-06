50293279488bfadcb10735a684e509c7d2edd4b0
Farindra <farindrabahadurbhandari@gmail.com>
2025-07-06 17:12:21 +0545

    Refactor user slice to define initial state interface and improve structure

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -1,16 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
-    name:  "userSlice",
-    initialState: {
-        name: "Farindra",
-        address:"KTM"
-    },
-    reducers: {
-        setName:(state)=>{
-            state.name === "farindra" ? "farindra" : "Jivan"
-        }
-    }
+interface IUserInitialState {
+  name: string;
+  address: string;
 }
 
-)
+const userInitialState: IUserInitialState = {
+  name: "Farindra",
+  address: "KTM",
+};
+const usersSlice = createSlice({
+  name: "userSlice",
+  initialState: userInitialState,
+  reducers: {
+    setName: (state) => {
+      state.name === "farindra" ? "farindra" : "Jivan";
+    },
+  },
+});
+
+const { setName } = usersSlice.actions; // calling setNames
+export default usersSlice.reducer;
