08feb930f3ac07deedc953cac96f39db805ca85f
Farindra <farindrabahadurbhandari@gmail.com>
2025-07-11 23:06:34 +0545

    redux Usedispatch, and usesSelector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+'use client'
+
+import { setAddress, setName } from "@/store/userSlice";
 import Image from "next/image";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Home() {
+  let name = "farindra"
+  let address = "Miklajung"
+
+  const dispatch = useDispatch()
+  const data = useSelector((store:any) => store.user)
+  console.log(`============>${data.Username, data.address}`)
+  dispatch(setName(name))
+  dispatch(setAddress(address))
+  setName(name)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -22,7 +35,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Save and see your changes {data.Username}
           </li>
         </ol>
 
--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -1,23 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface IUserInitialState {
-  name: string;
+  Username: string;
   address: string;
 }
 
 const userInitialState: IUserInitialState = {
-  name: "Farindra",
+  Username: "Farindra",
   address: "KTM",
 };
 const usersSlice = createSlice({
   name: "userSlice",
   initialState: userInitialState,
-  reducers: {
-    setName: (state) => {
-      state.name === "farindra" ? "farindra" : "Jivan";
+  reducers:{
+    setName:(state, action:PayloadAction<string>)=> {
+      state.Username = action.payload
     },
-  },
+
+
+    setAddress:(state, action:PayloadAction<string>)=> {
+      state.address = action.payload
+    }
+  }
 });
 
-const { setName } = usersSlice.actions; // calling setNames
+const { setName, setAddress } = usersSlice.actions; // calling setName, setAddresss
 export default usersSlice.reducer;
+export {setName, setAddress}
